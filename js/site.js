384a8a4ce09513e7e414bf4dde54063e43ae70e7
mistabenjo <benakehurst@hotmail.com>
2017-01-12 19:25:53 +0200

    Nearly done

--- js/site.js
+++ js/site.js
@@ -4,13 +4,46 @@
 
 	$(function(){
 
+		$("#aAboutMe").on("click", function( e ) {
 
+		    e.preventDefault();
 
+		    $("body, html").animate({ 
+		      scrollTop: $( $(this).attr('href') ).offset().top 
+		    }, 500);
 
+		});
+
+		$("#aProjects").on("click", function( e ) {
+
+		    e.preventDefault();
+
+		    $("body, html").animate({ 
+		      scrollTop: $( $(this).attr('href') ).offset().top 
+		    }, 500);
+
+		});
+
+		$("#aContact").on("click", function( e ) {
+
+		    e.preventDefault();
+
+		    $("body, html").animate({ 
+		      scrollTop: $( $(this).attr('href') ).offset().top 
+		    }, 500);
+
+		});
+
+		$("#contactMail").click(function(){
+			sendEmailToBen();
+		});
 
 	}); //End of jQuery Self Invoked
 
 	//Functions
 
+function sendEmailToBen(){
+	window.open('mailto:hi@benakehurst.online?subject=Hi Ben!');
+}
 
 })(); //End of JavaScript Self Invoked
