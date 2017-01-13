288752a343acb4410fd2990a76cfbf1d7fdd02f5
mistabenjo <benakehurst@hotmail.com>
2017-01-13 11:56:45 +0200

    added more projects and fixed links

--- js/site.js
+++ js/site.js
@@ -34,6 +34,16 @@
 
 		});
 
+		$("#lookBelow").on("click", function( e ) {
+
+		    e.preventDefault();
+
+		    $("body, html").animate({ 
+		      scrollTop: $( $(this).attr('href') ).offset().top 
+		    }, 500);
+
+		});
+
 		$("#contactMail").click(function(){
 			sendEmailToBen();
 		});
